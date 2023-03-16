47b42c076f0a674b092950a5e276ae9b9e5a0bd9
RachitSharma2001 <rachitsharma613@gmail.com>
2023-03-15 19:36:16 -0700

    Tests

--- e2e_test2.sh
+++ e2e_test2.sh
@@ -4,7 +4,6 @@ dd if=/dev/urandom of=test_file bs=4096 count=1
 ./fs_make.x disk_for_our_fs2.fs 100
 ./fs_make.x disk_for_ref_fs2.fs 100
 echo -e "$(<scripts/example2.script )" > testscript
-cat testscript
 ./test_fs.x script disk_for_our_fs2.fs testscript > given_output
 ./fs_ref.x script disk_for_ref_fs2.fs testscript > ref_output
 DIFF=$(diff given_output ref_output) 
