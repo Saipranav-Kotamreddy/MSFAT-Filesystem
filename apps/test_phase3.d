d420fb167dc3cc3e9e3a93362407fe058d2bab1c
RachitSharma2001 <rachitsharma613@gmail.com>
2023-03-17 08:33:07 -0700

    Submit

--- apps/test_phase3.d
+++
@@ -1,1 +0,0 @@
-test_phase3.o: test_phase3.c ../libfs/fs.h
--- unit_tests.sh
+++
@@ -1,5 +0,0 @@
-./test_phase1to3.sh | tail -1
-./test_phase4.sh | tail -1
-./test_phase4_err.sh | tail -1
-./test_phase4_read.sh | tail -1
-echo "Unit Tests all pass!"
